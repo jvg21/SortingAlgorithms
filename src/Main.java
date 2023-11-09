6a894f71e2399b8a89b6972c002e86d00e782c63
jvg21 <joaovitorgregorini@gmail.com>
2023-11-09 15:33:18 -0300

    teste2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SortingClass sortingclass = new SortingClass();
+        SortingClass sortingclass = new SortingClass(new int[]{2, 4, 6, 8, 10});
 
 
     }

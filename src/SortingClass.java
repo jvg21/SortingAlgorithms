6d315adfdafac51ab612b1086c3daf9ce2a20411
João Gregorini <joaovgregorini@gmail.com>
2023-11-09 21:22:28 -0300

    pedido

--- src/SortingClass.java
+++ src/SortingClass.java
@@ -6,18 +6,39 @@ public class SortingClass {
     }
 
     public int[] bubbleSort(){
+        System.out.println("-----------------Bubble Sort--------------------");
         int temp = 0;
         int[] sortedArray = this.valuesArray;
         for (int i = 0; i < sortedArray.length;i++){
+            int numTrades = 0;
             for (int j = 0; j < sortedArray.length-1; j++){
                 if (sortedArray[j]>sortedArray[j+1]){
                     temp = sortedArray[j+1];
                     sortedArray[j+1] = sortedArray[j];
                     sortedArray[j] = temp;
+                    numTrades++;
                 }
             }
-            System.out.println(sortedArray.toString());
+            if (numTrades == 0){
+                break;
+            }
+            for (int k = 0; k<sortedArray.length;k++){
+                System.out.print(sortedArray[k]+",");
+            }
+
+            System.out.println("\n");
         }
+        System.out.println("-------------------------------------------------------------");
         return sortedArray;
     }
+
+    public int[] insertionSort(){
+        int count = 1;
+        int[] sortedArray 
+        while (count<)
+        return sortedArray;
+    }
+//    public int[] quickSort(){
+//
+//    }
 }
